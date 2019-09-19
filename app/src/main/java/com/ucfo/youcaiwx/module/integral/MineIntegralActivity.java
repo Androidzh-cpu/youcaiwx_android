7949ef842890549f576024f6108d90e059b26503
Devil-AND <34635313+Devil-AND@users.noreply.github.com>
2019-09-19 09:08:25 +0800

    修改目录结构

--- app/src/main/java/com/ucfo/youcaiwx/module/integral/MineIntegralActivity.java
+++ app/src/main/java/com/ucfo/youcaiwx/module/integral/MineIntegralActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import com.ucfo.youcaiwx.R;
 import com.ucfo.youcaiwx.base.BaseActivity;
 
+/**
+ * Author: AND
+ * Time: 2019-9-19 上午 9:08
+ * Package: com.ucfo.youcaiwx.module.integral
+ * FileName: MineIntegralActivity
+ * ORG: www.youcaiwx.com
+ * Description:TODO 我的积分首页
+ */
 public class MineIntegralActivity extends BaseActivity {
     @Override
     protected int setContentView() {
