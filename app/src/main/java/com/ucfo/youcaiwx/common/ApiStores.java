e884b45012ff0d818ab67b7e9909f3fda8af95fa
Devil-AND <34635313+Devil-AND@users.noreply.github.com>
2019-11-28 10:31:50 +0800

    锁屏已添加

--- app/src/main/java/com/ucfo/youcaiwx/common/ApiStores.java
+++ app/src/main/java/com/ucfo/youcaiwx/common/ApiStores.java
@@ -87,7 +87,7 @@ public class ApiStores {
     public static final String QUESTION_GETPROJECT = BASE_URL + "Question/getProject";//获取题库展示专业
     public static final String QUESTION_GETPROJECTINFO = BASE_URL + "Question/questionIndex";//获取用户对应题库信息
     public static final String QUESTION_GETQUSTIONONRECORD = BASE_URL + "Question/questionRecord";//获取用户答题记录
-    public static final String QUESTION_AbilityTOAssess = "https://www.youcaiwx.cn/capacity-assessment-app";//用户能力评估
+    public static final String QUESTION_AbilityTOAssess = "https://www.youcaiwx.cn/#/capacity-assessment-app";//用户能力评估
     //public static final String QUESTION_AbilityTOAssess = "http://ycapi.youcaiwx.com/home/index.html#/capacity-assessment-app";//用户能力评估
     //http://ycapi.youcaiwx.com/home/index.html#/capacity-assessment-app?course_id=1&user_id=67
     //public static final String QUESTION_AbilityTOAssess = "http://192.168.3.23:8080/#/capacity-assessment-app";//用户能力评估
--- app/src/main/java/com/ucfo/youcaiwx/module/course/player/VideoPlayPageActivity.java
+++ app/src/main/java/com/ucfo/youcaiwx/module/course/player/VideoPlayPageActivity.java
@@ -1254,11 +1254,12 @@ public class VideoPlayPageActivity extends AppCompatActivity implements SurfaceH
         setLayoutVisibility(false, false);
         mIsFullScreenLocked = lockScreen;
         if (mIsFullScreenLocked) {
-            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.icon_locked);
+            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_icon_locked);
             playerLockedscreen.setImageDrawable(drawable);
             setLayoutVisibility(false, false);
+            ToastUtil.showBottomShortText(this, getResources().getString(R.string.course_lockedScreen));
         } else {
-            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.icon_unlocked);
+            Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_icon_unlocked);
             playerLockedscreen.setImageDrawable(drawable);
             setLayoutVisibility(true, true);
         }
@@ -1844,7 +1845,7 @@ public class VideoPlayPageActivity extends AppCompatActivity implements SurfaceH
         int playerBottomlinerHeight = AppUtils.getViewHeight(playerBottomliner);
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
         params.addRule(RelativeLayout.CENTER_IN_PARENT);
-        params.setMargins(DensityUtil.dip2px(this, 100), playerToplinerHeight, 0, playerBottomlinerHeight);
+        params.setMargins(DensityUtil.dip2px(this, 80), playerToplinerHeight, 0, playerBottomlinerHeight);
         playerRelativelayout.addView(mGestureView, params);//添加到布局中
 
         //设置手势监听
